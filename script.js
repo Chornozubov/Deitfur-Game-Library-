1f41f17e3a80756311386b8f0612d1b4d71736be
zzZeyjQ <denizshenko.denis@gmail.com>
2026-09-15 19:21:24 +0200

    daily update

--- script.js
+++ script.js
@@ -14,9 +14,37 @@ const games = [
 games.forEach((game, index) => {
   console.log(`${index + 1}. Game: "${game.name}" | genre: ${game.genre} | releaseYear: ${game.releaseYear}`);
 });
-function getGameByYear(year) {
-    return games.filter(game => game.releaseYear === year);
- console.log(getGamesByYear(2018));
- console.log(getGamesByYear(2012));
- console.log(getGamesByYear(2022));
+
+function getGamesByYear(year) {
+  return games.filter(game => game.releaseYear === year);
 }
+
+console.log(getGamesByYear(2018));
+console.log(getGamesByYear(2012));
+console.log(getGamesByYear(2022));
+
+function searchGame(query) {
+  return games.find(game =>
+    game.name.toLowerCase().includes(query.toLowerCase())
+  );
+}
+ console.log(searchGame("dota"));
+ console.log(searchGame("witcher"));
+ console.log(searchGame("counter"));
+ console.log(searchGame("elden"));
+ console.log(searchGame("forest"));
+ console.log(searchGame("red dead"));
+ console.log(searchGame("detroit"));
+ console.log(searchGame("mechanic"));
+ console.log(searchGame("cs:go")); 
+ console.log(searchGame("valheim"));
+ const names = games.map(game => game.name);
+console.log(names);
+const genres = games.map(game => game.genre);
+console.log(genres);
+const releaseYears = games.map(game => game.releaseYear);
+console.log(releaseYears);
+const uniqueGenres = [...new Set(genres)];
+console.log(uniqueGenres);
+const sortedByName = [...games].sort((a, b) => a.name.localeCompare(b.name));
+console.log(sortedByName);
