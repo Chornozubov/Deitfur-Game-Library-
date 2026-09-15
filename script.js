865a75dfe8d1288ee61de0806392f208354262bc
zzZeyjQ <denizshenko.denis@gmail.com>
2026-09-15 18:01:08 +0200

    daily update

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-const games = [ 
-    { name: "Dota 2" , genre: "MOBA", realeaseYear: 2013 },
-    { name: "Counter-Strike: Global Offensive", genre: "FPS", realeaseYear: 2012 },
-    { name: "The Witcher 3: Wild Hunt", genre: "RPG", realeaseYear: 2015 },
-    { name: "Elden Ring" , genre: "Action/RPG", realeaseYear: 2022 },
-    { name: "The Forest " , genre: "Survival", realeaseYear: 2018 },
-    { name: "Read Dead Redemption 2" , genre: "Action/Adventure", realeaseYear: 2018 },
-    { name: "Detroit: Become Human" , genre: "Interactive Drama", realeaseYear: 2018 },
-    { name: "Car Mechanic Simulator" , genre: "Simulation", realeaseYear: 2018 },
-    { name: "CS:GO" , genre: "FPS", realeaseYear: 2012 },
-    { name: "Valheim", genre: "Survival", realeaseYear: 2021 },
-];
- games.forEach((game, index) => {
-    console.log(`${index + 1}. Game: "${game.name}" | genre: ${game.genre} | releaseYear: ${game.realeaseYear}`);
-});
 const games = [
-    { name: "Dota 2" },
-    { name: "Counter-Strike: Global Offensive" },
-    { name: "The Witcher 3: Wild Hunt" },
-    { name: "Elden Ring" },
-    { name: "The Forest" },
-    { name: "Read Dead Redemption 2" },
-    { name: "Detroit: Become Human" },
-    { name: "Car Mechanic Simulator"},
-    { name: "CS:GO" },
-    { name: "Valheim" }
+  { name: "Dota 2", genre: "MOBA", releaseYear: 2013 },
+  { name: "Counter-Strike: Global Offensive", genre: "FPS", releaseYear: 2012 },
+  { name: "The Witcher 3: Wild Hunt", genre: "RPG", releaseYear: 2015 },
+  { name: "Elden Ring", genre: "Action/RPG", releaseYear: 2022 },
+  { name: "The Forest", genre: "Survival", releaseYear: 2018 },
+  { name: "Red Dead Redemption 2", genre: "Action/Adventure", releaseYear: 2018 },
+  { name: "Detroit: Become Human", genre: "Interactive Drama", releaseYear: 2018 },
+  { name: "Car Mechanic Simulator", genre: "Simulation", releaseYear: 2018 },
+  { name: "CS:GO", genre: "FPS", releaseYear: 2012 },
+  { name: "Valheim", genre: "Survival", releaseYear: 2021 },
 ];
+
+games.forEach((game, index) => {
+  console.log(`${index + 1}. Game: "${game.name}" | genre: ${game.genre} | releaseYear: ${game.releaseYear}`);
+});
+function getGameByYear(year) {
+    return games.filter(game => game.releaseYear === year);
+ console.log(getGamesByYear(2018));
+ console.log(getGamesByYear(2012));
+ console.log(getGamesByYear(2022));
+}
