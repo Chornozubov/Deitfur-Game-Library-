d58fb3d2e4f15f335466f1765f096d580fdafc89
zzZeyjQ <denizshenko.denis@gmail.com>
2026-09-16 19:20:20 +0200

    saving when restarting

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-console.log(games[0].name);
-console.log(games[1].genre);
-const games = [
+const defaultGames = [
   { name: "Dota 2", genre: "MOBA", releaseYear: 2013 },
   { name: "Counter-Strike: Global Offensive", genre: "FPS", releaseYear: 2012 },
   { name: "The Witcher 3: Wild Hunt", genre: "RPG", releaseYear: 2015 },
@@ -13,6 +11,13 @@ const games = [
   { name: "Valheim", genre: "Survival", releaseYear: 2021 },
 ];
 
+const savedGames = localStorage.getItem("games");
+const games = savedGames
+  ? JSON.parse(savedGames)
+  : defaultGames;
+function saveGames() {
+  localStorage.setItem("games", JSON.stringify(games));
+}
 games.forEach((game, index) => {
   console.log(`${index + 1}. Game: "${game.name}" | genre: ${game.genre} | releaseYear: ${game.releaseYear}`);
 });
@@ -30,23 +35,35 @@ function searchGame(query) {
     game.name.toLowerCase().includes(query.toLowerCase())
   );
 }
- console.log(searchGame("dota"));
- console.log(searchGame("witcher"));
- console.log(searchGame("counter"));
- console.log(searchGame("elden"));
- console.log(searchGame("forest"));
- console.log(searchGame("red dead"));
- console.log(searchGame("detroit"));
- console.log(searchGame("mechanic"));
- console.log(searchGame("cs:go")); 
- console.log(searchGame("valheim"));
- const names = games.map(game => game.name);
+console.log(searchGame("dota"));
+console.log(searchGame("witcher"));
+console.log(searchGame("counter"));
+console.log(searchGame("elden"));
+console.log(searchGame("forest"));
+console.log(searchGame("red dead"));
+console.log(searchGame("detroit"));
+console.log(searchGame("mechanic"));
+console.log(searchGame("cs:go"));
+console.log(searchGame("valheim"));
+
+const names = games.map(game => game.name);
 console.log(names);
+
 const genres = games.map(game => game.genre);
 console.log(genres);
+
 const releaseYears = games.map(game => game.releaseYear);
 console.log(releaseYears);
+
 const uniqueGenres = [...new Set(genres)];
 console.log(uniqueGenres);
-const sortedByName = [...games].sort((a, b) => a.name.localeCompare(b.name));
-console.log(sortedByName); 
+
+const sortedByName = [...games].sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
+console.log(sortedByName);
+
+function addGame(game) {
+  games.push(game);
+  saveGames();
+}
